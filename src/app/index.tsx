0415a3734d5eb8a7b71e08963be9d2c1c2e7a591
Christian Rodrigues Batista <chris.rod_batista@outlook.com>
2024-02-12 22:43:21 -0300

    Feat: Components category-button and product

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,61 @@
-import { View } from 'react-native';
-import { Header } from "../components/header";
+import { useRef, useState } from "react";
+import { View, FlatList, SectionList, Text } from 'react-native';
+
+import { CATEGORIES, MENU } from "@/utils/data/products";
+
+import { Header } from "@/components/header";
+import { CategoryButton } from "@/components/category-button";
+import { Product } from "@/components/product";
 
 export default function Home(){
+  const [category, setCategory] = useState(CATEGORIES[0])
+
+  const sectionListRef = useRef<SectionList>(null)
+
+  function handleCategorySelect(selectedCategory: string) {
+    setCategory(selectedCategory);
+    const sectionIndex = CATEGORIES.findIndex((category) => category === selectedCategory);
+
+    if(sectionListRef.current) {
+      sectionListRef.current.scrollToLocation({
+        animated: true,
+        sectionIndex,
+        itemIndex: 0
+      })
+    }
+  }
+
   return (
     <View className="flex-1 pt-8">
-      <Header title="Faça seu pedido" cartQuatityItems={3}/>
+      <Header title="Make an order" cartQuatityItems={3}/>
+
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <CategoryButton title={item} isSelected={item === category} onPress={() => handleCategorySelect(item)}/>}
+        horizontal
+        className="max-h-10 mt-5"
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20}}
+      />
+
+      <SectionList  
+        ref={sectionListRef}
+        sections={MENU}
+        keyExtractor={(item) => item.id}
+        stickySectionHeadersEnabled={false}
+        renderItem={({ item }) => (
+          <Product data={item}/>
+        )}
+        renderSectionHeader={({section: {title}}) => (
+          <Text className="text-xl text-white front-heading mt-8 mb-3">
+            {title}
+          </Text>
+        )}
+        className="flex-1 p-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      />
     </View>   
   )
 }
